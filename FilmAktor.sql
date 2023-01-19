087756fd92517dd0ee434b152cb41c2366a36fdd
Mateusz <mgmateuszgalazka860@gmail.com>
2023-01-19 23:10:00 +0100

    M

---
+++ FilmAktor.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[FilmAktor] (
+    [FilmID]    INT        NOT NULL,
+    [AktorID]   INT        NOT NULL,
+    [NazwaRoli] NVARCHAR (100) NULL,
+    CONSTRAINT [PK_FilmAktor] PRIMARY KEY CLUSTERED ([FilmID] ASC, [AktorID] ASC),
+    CONSTRAINT [FK_FilmAktor_Aktor] FOREIGN KEY ([AktorID]) REFERENCES [dbo].[Aktor] ([AktorID]),
+    CONSTRAINT [FK_FilmAktor_Film] FOREIGN KEY ([FilmID]) REFERENCES [dbo].[Film] ([FilmID])
+);
+
+
+GO   
+
+ 

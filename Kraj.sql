126bb1019b921999c504a1e2c2e32ccba7ebf0ff
Mateusz <mgmateuszgalazka860@gmail.com>
2023-01-19 23:12:31 +0100

    C2

--- Kraj.sql
+++ Kraj.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Kraj] (
     [KrajID] INT            IDENTITY (1, 1) NOT NULL,
-    [Kraj]   NVARCHAR (150) NOT NULL,
+    [Kraj]   NVARCHAR (20) NOT NULL,
     PRIMARY KEY CLUSTERED ([KrajID] ASC)
 );
 
